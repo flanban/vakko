b726c9d9202a084e4bd7ededd451a1dbe35d2bc1
flanban <johnsfuller@gmail.com>
2014-05-13 19:55:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vakko::Application.config.secret_token = '85415ef3323c6d7373c22894ab17a1956cf03cc37f29ce342e45c692a7c64ffd86370aff551b026e9bd8a821fb6ec776d51a17e2d98b6213b5fb3ce07080c031'
