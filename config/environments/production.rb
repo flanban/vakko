40a96ccbe3f1924044a85c346dee850263ef1ac0
flanban <johnsfuller@gmail.com>
2014-05-13 20:03:01 -0400

    add basic auth to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Vakko::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.middleware.insert_after(::Rack::Runtime, "::Rack::Auth::Basic", "Staging") do |u, p|
+     [u, p] == ['test', 'qu1n1n3']
+  end
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
