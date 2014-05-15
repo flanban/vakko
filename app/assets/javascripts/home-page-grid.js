2a65ac1e31f8d2512f810e5e04388140960cadb4
flanban <johnsfuller@gmail.com>
2014-05-15 08:41:36 -0400

    round the numbers

--- app/assets/javascripts/home-page-grid.js
+++ app/assets/javascripts/home-page-grid.js
@@ -10,22 +10,22 @@ $(function() {
 
   function setAspectRation0() {
     entryWidth = $('.aspect-ratio-0').width();
-    entryHeight = $('.aspect-ratio-0').width() / 1.6;//that weird number is the img's aspect ratio in decimal form
+    entryHeight = Math.round($('.aspect-ratio-0').width() / 1.6);//that weird number is the img's aspect ratio in decimal form
     $('.aspect-ratio-0').height(entryHeight);
   }
   function setAspectRation1() {
     entryWidth = $('.aspect-ratio-1').width();
-    entryHeight = $('.aspect-ratio-1').width() / .85;//that weird number is the img's aspect ratio in decimal form
+    entryHeight = Math.round($('.aspect-ratio-1').width() / .85);//that weird number is the img's aspect ratio in decimal form
     $('.aspect-ratio-1').height(entryHeight -1);
   }
   function setAspectRation2() {
     entryWidth = $('.aspect-ratio-2').width();
-    entryHeight = $('.aspect-ratio-2').width() / 1.7;//that weird number is the img's aspect ratio in decimal form
+    entryHeight = Math.round($('.aspect-ratio-2').width() / 1.7);//that weird number is the img's aspect ratio in decimal form
     $('.aspect-ratio-2').height(entryHeight -1);
   }
   function setAspectRation3() {
     entryWidth = $('.aspect-ratio-3').width();
-    entryHeight = $('.aspect-ratio-3').width() / .425;//that weird number is the img's aspect ratio in decimal form
+    entryHeight = Math.round($('.aspect-ratio-3').width() / .425);//that weird number is the img's aspect ratio in decimal form
     $('.aspect-ratio-3').height(entryHeight -1);
   }
   function setAspectRatios() {
