6b9ce1aa5097049037e1e7410d46054c960ac13d
flanban <johnsfuller@gmail.com>
2014-05-19 18:06:13 -0400

    set mobile aspect ratios

--- app/assets/javascripts/home-page-grid.js
+++ app/assets/javascripts/home-page-grid.js
@@ -7,20 +7,37 @@ $(function() {
     $('#content-area').width(contentWidth);
   }
 
+
   function setAspectRation0() {
     entryWidth = $('.aspect-ratio-0').width();
     entryHeight = Math.round($('.aspect-ratio-0').width() / 1.6);//that weird number is the img's aspect ratio in decimal form
-    $('.aspect-ratio-0').height(entryHeight);
+    entryHeightMobile = Math.round($('.aspect-ratio-0').width() / .8);//that weird number is the img's aspect ratio in decimal form
+    if($(window).width()<768){
+      $('.aspect-ratio-0').height(entryHeightMobile);
+    } else {
+      $('.aspect-ratio-0').height(entryHeight -1);
+    }
   }
   function setAspectRation1() {
     entryWidth = $('.aspect-ratio-1').width();
     entryHeight = Math.round($('.aspect-ratio-1').width() / .85);//that weird number is the img's aspect ratio in decimal form
-    $('.aspect-ratio-1').height(entryHeight -1);
+    entryHeightMobile = Math.round($('.aspect-ratio-1').width() / 1);//that weird number is the img's aspect ratio in decimal form
+    if($(window).width()<768){
+      $('.aspect-ratio-1').height(entryHeightMobile);
+    } else {
+      $('.aspect-ratio-1').height(entryHeight -1);
+    }
   }
   function setAspectRation2() {
     entryWidth = $('.aspect-ratio-2').width();
     entryHeight = Math.round($('.aspect-ratio-2').width() / 1.7);//that weird number is the img's aspect ratio in decimal form
-    $('.aspect-ratio-2').height(entryHeight -1);
+    entryHeightMobile = Math.round($('.aspect-ratio-2').width() / 1);//that weird number is the img's aspect ratio in decimal form
+
+    if($(window).width()<768){
+      $('.aspect-ratio-2').height(entryHeightMobile);
+    } else {
+      $('.aspect-ratio-2').height(entryHeight -1);
+    }
   }
   function setAspectRation3() {
     entryWidth = $('.aspect-ratio-3').width();
