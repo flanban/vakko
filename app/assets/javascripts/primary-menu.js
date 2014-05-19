6a9dd2b5add9f64861804443f0007765489ad5f2
flanban <johnsfuller@gmail.com>
2014-05-19 17:29:12 -0400

    match image styles to mock

--- app/assets/javascripts/primary-menu.js
+++ app/assets/javascripts/primary-menu.js
@@ -1,5 +1,6 @@
 $(function() {
-  $('#mobile-nav-toggle').click(function() {
+  $('#mobile-nav-toggle').click(function(e) {
+    e.preventDefault();
     $('#nav-items').slideToggle();
     $('html').toggleClass('mobile-nav-open');
   });
