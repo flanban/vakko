2bb886c2a0e849698e6fe904f29323db4a350252
flanban <johnsfuller@gmail.com>
2014-05-20 10:51:52 -0400

    precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Vakko::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
