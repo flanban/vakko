b9ab64ddaab74d9fa29ee3a9bb03034f988e7b2b
flanban <johnsfuller@gmail.com>
2014-05-22 13:44:32 -0400

    make images fade in

--- app/assets/javascripts/_collection.js
+++ app/assets/javascripts/_collection.js
@@ -10,4 +10,27 @@ $(function() {
     $('.info-slideout').css('margin-right', 0);
     $('html').addClass('slide-info-open');
   });
+  
+  
+  function fadeInContent() {
+    currContentObject = $('.collection-grid a')
+    var css = {
+      visibility: 'visible',
+      opacity: 0
+    };
+    css[self._vendorPref + 'transition'] = 'opacity 600ms ease-in-out';
+    
+    currContentObject.css(css);
+
+    setTimeout(function() {
+      currContentObject.css('opacity', 1);
+    }, 16);
+  }
+
+ // fadeInContent();
+});
+$(document).ready(function(){
+  setTimeout(function() {
+    $(".collection-grid a img").hide(0).delay(500).fadeIn(400);
+  }, 16);
 });
