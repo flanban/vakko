5e05e16d7e67cb7f5e4555987f963018adbd699a
flanban <johnsfuller@gmail.com>
2014-06-07 18:35:04 -0700

    precompile

---
+++ public/assets/static_pages/lookbook_subnav-df45c33075a0febf2971a46ed1b5730b.js
@@ -0,0 +1,1 @@
+$(function(){$(".collection.has-subnav").stop(!0,!0).hover(function(){$("#subnav-menu").addClass("hover")},function(){setTimeout(function(){$("#subnav-menu").removeClass("hover")},1e3)}),$("#subnav-menu").stop(!0,!0).hover(function(){$(this).addClass("active")},function(){setTimeout(function(){$("#subnav-menu").removeClass("hover active")},100)})});
