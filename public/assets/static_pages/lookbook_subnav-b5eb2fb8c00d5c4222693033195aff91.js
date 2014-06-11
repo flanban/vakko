ce1cfb919837bf7d0e0e36ba07b8c03686401b80
flanban <johnsfuller@gmail.com>
2014-06-10 18:53:40 -0700

    chocolate responsive

--- public/assets/static_pages/lookbook_subnav-b5eb2fb8c00d5c4222693033195aff91.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(window).width()>768&&($(".collection.has-subnav").stop(!0,!0).hover(function(){$("#subnav-menu").addClass("hover")},function(){setTimeout(function(){$("#subnav-menu").removeClass("hover")},1e3)}),$("#subnav-menu").stop(!0,!0).hover(function(){$(this).addClass("active")},function(){setTimeout(function(){$("#subnav-menu").removeClass("hover active")},100)}))});
