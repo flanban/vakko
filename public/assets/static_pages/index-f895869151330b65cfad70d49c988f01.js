6d7419dfb20ae29db535e37e8e883f657a9a9cef
flanban <johnsfuller@gmail.com>
2014-06-13 09:51:25 -0700

    preds

---
+++ public/assets/static_pages/index-f895869151330b65cfad70d49c988f01.js
@@ -0,0 +1,1 @@
+$(document).ready(function(a){a(".primary-slider").royalSlider({keyboardNavEnabled:!0,imageScalePadding:0,arrowsNav:!1,controlNavigation:"bullets",navigateByClick:!1,usePreloader:!1,slidesSpacing:0,fadeinLoadedSlide:!0,addActiveClass:!0,arrowsNavAutoHide:!1,controlsInside:!0})});
