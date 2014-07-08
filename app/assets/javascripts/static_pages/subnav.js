28a30cf5d6f53df4af69e090b3d48acf73c5bac2
flanban <johnsfuller@gmail.com>
2014-07-08 12:14:50 -0700

    nav, wip

--- app/assets/javascripts/static_pages/subnav.js
+++ app/assets/javascripts/static_pages/subnav.js
@@ -1,10 +1,59 @@
+function showMensNav() {
+ // $('#subnav-menu-mens').stop(true).css('visibility','visible');
+ $('#subnav-menu-mens').stop(true).fadeTo(200, 1);
+}
+function showWomensNav() {
+  //$('#subnav-menu-womens').stop(true).css('visibility','visible')
+  $('#subnav-menu-womens').stop(true).fadeTo(200, 1);
+}
+function showHeadquartersNav() {
+  //$('#subnav-menu-headquarters').stop(true).css('visibility','visible');
+  $('#subnav-menu-headquarters').stop(true).fadeTo(200, 1);
+}
+function pickAndShowNav() {
+  if ($(this).hasClass('mens-nav')) {
+    showMensNav()
+  } else if ($(this).hasClass('womens-nav')) {
+    showWomensNav()
+  } else if ($(this).hasClass('headquarters-nav')) {
+    showHeadquartersNav()
+  }
+}
+function hideNav() {
+  $('#subnav-menu-mens, #subnav-menu-womens, #subnav-menu-headquarters').stop(true).fadeTo(700, 0, function() {
+  });
+}
+
 $(function() {
+
+  
   if($(window).width()>768){
     // show subnav when you rollover main menu item
-    $(".collection.has-subnav").hover(function() {
-      $('#subnav-menu').stop(true).fadeTo(200, 1);
+    $(".has-subnav").hover(function() {
+      pickAndShowNav.apply(this)
     }, function() {
-      $('#subnav-menu').stop(true).fadeTo(200, 0);
-    });
+          hideNav();
+          setTimeout(function(){
+            //alert(1)
+          }, 5000);
+        });
   }
+  
+  // make sure it stays showing
+   $('.subnav-menu').stop( true, true ).hover(
+      function() {
+       // alert(1)
+        //hideNav().stop(true)
+        //showMensNav()
+       // alert($('#mens-subnav-menu').text())
+        //$('#mens-subnav-menu').stop(true, true).fadeTo(200, 1);
+        //$('#mens-subnav-menu').stop(true, true).css('opcatiy', '1!important')
+      }, function() {
+        //hideNav();
+        
+    /*    setTimeout(function(){
+          hideNav();
+          //alert(1)
+        }, 100); */
+    });
 });
