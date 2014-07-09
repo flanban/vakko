1db6231f10d9f5c5ce9db642f23ab7244e972d9c
flanban <johnsfuller@gmail.com>
2014-07-08 19:03:41 -0700

    fix nav toggle, hide preloader

--- app/assets/javascripts/shared/primary-menu.js
+++ app/assets/javascripts/shared/primary-menu.js
@@ -1,13 +1,14 @@
 $(function() {
  
   
-  $('body').on('click', ' .headquarters-nav', function (e) {
+  $('body').on('click', ' .headquarters-nav, #mobile-nav-toggle', function (e) {
     e.preventDefault()
   })
   
+
   $('body').on('click', '#mobile-nav-toggle > img', function (e) {
     e.preventDefault();
-   $('html').toggleClass('mobile-nav-open');
+   $(this).toggleClass('mobile-nav-open');
   })
   
 });
