4dc1da6a3749e8f5b500c181438f84e22006e794
flanban <johnsfuller@gmail.com>
2014-09-03 13:14:04 -0700

    text and image edits

--- app/assets/javascripts/static_pages/index.js
+++ app/assets/javascripts/static_pages/index.js
@@ -4,6 +4,22 @@ $(document).ready(function($) {
     // options
     keyboardNavEnabled: true,
     imageScalePadding: 0,
+    imageScaleMode: 'fill',
+    arrowsNav: false,
+    controlNavigation: false,
+    navigateByClick: false,
+    usePreloader: false,
+    slidesSpacing:0,
+    fadeinLoadedSlide: true,
+    addActiveClass: true,
+    arrowsNavAutoHide: false,
+    controlsInside: true
+  });
+  $('.home-slider').royalSlider({
+    // options
+    keyboardNavEnabled: true,
+    imageScalePadding: 0,
+    imageScaleMode: 'fill',
     arrowsNav: false,
     controlNavigation: 'bullets',
     navigateByClick: false,
