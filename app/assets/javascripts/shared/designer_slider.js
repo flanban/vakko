74aa33d4c8910b8b675e8c00637940e977247c72
flanban <johnsfuller@gmail.com>
2014-09-04 12:54:48 -0700

    clean assets

--- app/assets/javascripts/shared/designer_slider.js
+++ app/assets/javascripts/shared/designer_slider.js
@@ -1,15 +1,1 @@
-$('.lower-content-slider').royalSlider({
-  // options
-  keyboardNavEnabled: true,
-  controlNavigation: 'none',
-  imageScalePadding: 0,
-  imageScaleMode: 'fill',
-  arrowsNav: true,
-  navigateByClick: false,
-  usePreloader: false,
-  slidesSpacing:0,
-  fadeinLoadedSlide: true,
-  addActiveClass: true,
-  arrowsNavAutoHide: false,
-  controlsInside: true
-});
+alert(1)
